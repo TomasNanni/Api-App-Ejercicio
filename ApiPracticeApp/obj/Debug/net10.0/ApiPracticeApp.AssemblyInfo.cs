2ce5a996c14d9935f9e72adde55d6758e8c5c647
tomasNotebook <tomasnanni18@gmail.com>
2026-04-28 17:57:22 -0300

    hechas las referencias entre capas

--- ApiPracticeApp/obj/Debug/net10.0/ApiPracticeApp.AssemblyInfo.cs
+++ ApiPracticeApp/obj/Debug/net10.0/ApiPracticeApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiPracticeApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9b311210d76968dd12760175506ef22871719b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54941c870a111bd7dad86075e8a8ee1da0c24b18")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiPracticeApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiPracticeApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
